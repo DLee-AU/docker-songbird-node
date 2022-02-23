6dd5dd0290101e79c1f1e9bce8ae5623e7bfc5b1
Dustin Lee <DLee-AU@users.noreply.github.com>
2022-02-23 16:36:34 +1100

    Added Variabled in config.mk

--- make.d/config.mk
+++ make.d/config.mk
@@ -1,1 +1,18 @@
 # config.mk
+
+DOCKER=/usr/bin/docker
+PODMAN=/usr/bin/podman
+
+PYTHON_IMAGE=library/python:3.9
+PYTHON=${DOCKER} run ${PYTHON_IMAGE}
+
+# DOCKER-COMPOSE=/usr/bin/docker-compose
+DOCKER-COMPOSE=~/.pyenv/shims/docker-compose
+# PODMAN-COMPOSE=/usr/bin/podman-compose
+PODMAN-COMPOSE=~/.pyenv/shims/podman-compose
+
+
+# S3 Data Locations and Service
+S3_HOSTNAME="http://s3.url.example"
+S3_URI="s3-bucket-example"
+S3_URL=$(S3_HOSTNAME)/$(S3_URI)
